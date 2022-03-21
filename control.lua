1752879724f0af5923b7d6375068a2ab1b2d723f
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-03-20 17:58:50 -0700

    0.0.2

---
+++ control.lua
@@ -0,0 +1,15 @@
+function check_techs()
+  for _, force in pairs(game.forces) do
+    if force.technologies["discharge-defense-equipment"].researched then
+      force.recipes["automatic-discharge-defense-equipment"].enabled = true
+    end
+  end
+end
+
+script.on_init(function()
+  check_techs()
+end)
+
+script.on_configuration_changed(function()
+  check_techs()
+end)
